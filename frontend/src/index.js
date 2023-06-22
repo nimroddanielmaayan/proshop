819e377c7eefb6a1d702e4030ac38a75c8357f53
nimroddanielmaayan <nimrodmayan@gmail.com>
2023-06-22 19:01:19 +0300

    test commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,11 +10,14 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
@@ -22,7 +25,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 
