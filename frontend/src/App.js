4d5c66bf504ff3b2b10f1aaa0694dcc273adcc6b
nimroddanielmaayan <nimrodmayan@gmail.com>
2023-06-23 01:19:31 +0300

    daily progress update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,20 @@
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import products from '../products'
+import { useEffect, useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
@@ -14,6 +26,6 @@ const HomeScreen = () => {
         ))}
       </Row>
     </>
-  )
-}
-export default HomeScreen
+  );
+};
+export default HomeScreen;
